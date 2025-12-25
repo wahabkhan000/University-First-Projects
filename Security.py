0aa57d7d4a7a0c62e002e4f2646258cca80eda7a
Abdul Wahab Akbar <itswahab00119@gmail.com>
2025-12-25 21:26:00 +0500

    Create Security.py

---
+++ Security.py
@@ -0,0 +1,37 @@
+print("مَرْحَبًا يَا شَيْخُ\nEnter Password: \n")
+Password=input()
+Password1="Abdul Wahab 00112233@#$"
+while Password1 != Password:
+    print("=======Wrong password======= \nPlease try again: ")
+    Password=input()
+print("===وَاللهِ يَا حَبِيبِي، أَدْخَلْتَ كَلِمَةَ الْمُرُورِ الصَّحِيحَة===\nYou entered correct password ")
+print("Please enter the 'verification code':")
+code=input()
+code1="Wahab is a killer."
+while code != code1:
+    print("=======Wrong Password======= \nPlease try again: ")
+    code=input()
+print("===وَاللهِ يَا حَبِيبِي، أَدْخَلْتَ كَلِمَةَ الْمُرُورِ الصَّحِيحَة===\nYou entered correct code ")
+name=input("What's your name?\n")
+first,last=name.split(" ")
+first1="Abdul"
+last1="Wahab"
+while first != first1:
+    print("====Name doesn't match====\nPlease try again: ", )
+    first=input()
+while last != last1:
+    print("====Name doesn't match====\nPlease try again: ", )
+    last =input()
+print("وَاللهِ يَا حَبِيبِي، كَمْ عُمْرُكَ؟")
+Age=int(input("What's your age?\n"))
+Age1= int(19)
+while Age != Age1:
+    print("===Wrong Age===\n Please try again.")
+    Age=int(input("What's your age?"))
+print("===Woohoo!===")
+print("وَاللهِ يَا حَبِيبِي، مُبَارَكٌ")
+print("Access Granted")
+print("Enjoy Yourself")
+print("===وَاللهِ يَا حَبِيبِي، اِسْتَمْتِعْ بِوَقْتِكَ===")
+print("https://pin.it/64oPnw81e\n===By By!===")
+print("===وَاللهِ يَا حَبِيبِي، خُدَاحَافِظ!===")
